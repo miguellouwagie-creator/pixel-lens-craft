0e16891eca95ac47358a9693e40a803873d34202
Miguel Louwagie <miguellouwagie@gmail.com>
2026-04-26 10:17:20 +0200

    feat: wire ThemeProvider with dark default (closes DT-08)

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "next-themes";
 import { AuthProvider } from "@/contexts/AuthContext";
 import App from "./App.tsx";
 import "./index.css";
-import "./i18n/config"; // ← AÑADE ESTA LÍNEA
+import "./i18n/config";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </QueryClientProvider>,
+  <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </ThemeProvider>,
 );
